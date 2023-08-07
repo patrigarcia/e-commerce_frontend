86dcc3a801e54bc39ac213e9c6aeba1436675732
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-08-07 21:18:35 +0200

    Add initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import Login from "./components/Login/Login";
+import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
+import Header from "./components/Header/Header";
 
 const App = () => {
-    return <Login />;
+    return (
+        <Grid templateAreas={{ base: `"nav" "main" "footer"`, lg: `"nav nav" "aside main" "footer footer"` }} templateColumns={{ base: "1fr", lg: "200px 1fr" }}>
+            <GridItem area="nav" background="red">
+                <Header />
+            </GridItem>
+            <Show above="lg">
+                <GridItem area="aside" paddingX={5} background="blue">
+                    Aside
+                </GridItem>
+            </Show>
+            <GridItem area="main" background="green">
+                <Box paddingLeft={9} paddingRight={9}>
+                    Main
+                </Box>
+            </GridItem>
+            <GridItem area="footer" background="purple">
+                Footer
+            </GridItem>
+        </Grid>
+    );
 };
 
 export default App;
