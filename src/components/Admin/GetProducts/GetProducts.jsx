678e316b0afd59f670705a9e715372cc341ad242
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-08-25 21:21:14 +0200

    New changes

--- src/components/Admin/GetProducts/GetProducts.jsx
+++ src/components/Admin/GetProducts/GetProducts.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Box, Text, Table, Thead, Tbody, Tr, Th, Td, Flex } from "@chakra-ui/react";
 import apiClient from "../../../api/apiClient";
+import ReactPaginate from "react-paginate";
 import "./GetProducts.scss";
 
 const GetProducts = () => {
     const [products, setProducts] = useState([]);
     const [categoryMap, setCategoryMap] = useState({});
+    const [currentPage, setCurrentPage] = useState(0);
+    const productsPerPage = 10;
 
     useEffect(() => {
         async function fetchProducts() {
@@ -34,41 +37,52 @@ const GetProducts = () => {
         fetchCategories();
     }, []);
 
+    const indexOfLastProduct = (currentPage + 1) * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
     return (
-        <>
-            {" "}
-            <Flex>
-                <Text fontSize="20px" fontWeight="bold" mb={3}>
-                    Lista de Productos con Categorías
-                </Text>
-                <Box className="product_list">
-                    <Table className="product_table" variant="striped" colorScheme="purple">
-                        <Thead>
-                            <Tr>
-                                <Th>ID</Th>
-                                <Th>Nombre</Th>
-                                <Th>Descripción</Th>
-                                <Th>Categoría</Th>
-                                <Th>Stock</Th>
-                                <Th>Precio</Th>
+        <Flex direction="column">
+            <Text className="product_title">Lista de Productos</Text>
+            <Box className="product_list">
+                <Table variant="striped" colorScheme="purple">
+                    <Thead>
+                        <Tr>
+                            <Th>ID</Th>
+                            <Th>Nombre</Th>
+                            <Th>Descripción</Th>
+                            <Th>Categoría</Th>
+                            <Th>Stock</Th>
+                            <Th>Precio</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {currentProducts.map((product) => (
+                            <Tr key={product.id}>
+                                <Td>{product.id}</Td>
+                                <Td>{product.name}</Td>
+                                <Td>{product.description}</Td>
+                                <Td>{categoryMap[product.categoryId] || "Sin categoría"}</Td>
+                                <Td>{product.stock}</Td>
+                                <Td>€{product.price}</Td>
                             </Tr>
-                        </Thead>
-                        <Tbody>
-                            {products.map((product) => (
-                                <Tr key={product.id}>
-                                    <Td>{product.id}</Td>
-                                    <Td>{product.name}</Td>
-                                    <Td>{product.description}</Td>
-                                    <Td>{categoryMap[product.categoryId] || "Sin categoría"}</Td>
-                                    <Td>{product.stock}</Td>
-                                    <Td>€{product.price}</Td>
-                                </Tr>
-                            ))}
-                        </Tbody>
-                    </Table>
-                </Box>
-            </Flex>
-        </>
+                        ))}
+                    </Tbody>
+                </Table>
+            </Box>
+            <ReactPaginate
+                previousLabel={"Anterior"}
+                nextLabel={"Siguiente"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={Math.ceil(products.length / productsPerPage)}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={(selected) => setCurrentPage(selected.selected)}
+                containerClassName={"pagination"}
+                activeClassName={"active"}
+            />
+        </Flex>
     );
 };
 
