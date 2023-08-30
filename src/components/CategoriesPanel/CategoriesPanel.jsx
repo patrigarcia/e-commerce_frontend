4582a528c2dba30d1686011db24db5c17afa2b7f
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-08-30 23:58:12 +0200

    Add VER TODO menu in categories

--- src/components/CategoriesPanel/CategoriesPanel.jsx
+++ src/components/CategoriesPanel/CategoriesPanel.jsx
@@ -6,6 +6,7 @@ import useCategories from "../../hooks/useCategories";
 const CategoriesPanel = ({ onSelectCategory, selectedCategoryId }) => {
     const [categories, setCategories] = useState([]);
     const { fetchCategories } = useCategories();
+    const isSelectedCategoryNull = selectedCategoryId == null || selectedCategoryId == undefined;
 
     useEffect(() => {
         fetchCategories().then((res) => setCategories(res.data));
@@ -17,6 +18,22 @@ const CategoriesPanel = ({ onSelectCategory, selectedCategoryId }) => {
                 Categorías
             </Heading>
             <List styleType="none">
+                <ListItem>
+                    <HStack paddingY="10px" paddingX="2px">
+                        {isSelectedCategoryNull && <BiSolidGame />}
+                        <Button
+                            fontWeight={isSelectedCategoryNull ? "bold" : "normal"}
+                            colorScheme={isSelectedCategoryNull ? "purple" : "normal"}
+                            fontSize="lg"
+                            variant="link"
+                            onClick={() => onSelectCategory({ categoryId: null, categoryName: null })}
+                            whiteSpace="normal"
+                            textAlign="left"
+                        >
+                            VER TODO
+                        </Button>
+                    </HStack>
+                </ListItem>
                 {categories.map((category) => (
                     <ListItem key={category.id}>
                         <HStack paddingY="10px" paddingX="2px">
