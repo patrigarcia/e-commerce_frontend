bcf44e78b09f9e0f697606dbef364cddbed9f075
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-08-30 16:42:59 +0200

    Add new components

---
+++ src/assets/ImageBuy.jsx
@@ -0,0 +1,12 @@
+import { Box, Center, Image } from "@chakra-ui/react";
+import React from "react";
+
+const ImageBuy = () => {
+    return (
+        <Center pt="15%" mb="15vh">
+            <Image w="60vh" src="src/assets/pay.png" alt="Pagar" />
+        </Center>
+    );
+};
+
+export default ImageBuy;
--- src/components/Cart/Cart/Cart.jsx
+++ src/components/Cart/Cart/Cart.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { Card, Container, Flex, Grid } from "@chakra-ui/react";
+import { Flex, Grid } from "@chakra-ui/react";
 import StepperCart from "../StepperCart/StepperCart";
 import Checkout from "../Checkout/Checkout";
 import ProductsCart from "../ProductsCart/ProductsCart";
 import AdressCart from "../AdressCart/AdressCart";
 import OrderCompleted from "../../Orders/OrderCompleted/OrderCompleted";
 import "./Cart.scss";
+import ImageBuy from "../../../assets/ImageBuy";
 
 const Cart = () => {
     const [buyStep, setBuyStep] = useState(0);
@@ -19,6 +20,7 @@ const Cart = () => {
                     {buyStep === 0 && <ProductsCart />}
                     {buyStep === 1 && <AdressCart selection={() => setBuyStep(buyStep + 1)} updateAddress={() => setPendingAddress(false)} />}
 
+                    {buyStep === 2 && <ImageBuy />}
                     {[0, 1, 2].includes(buyStep) && <Checkout buyStep={buyStep} selection={() => setBuyStep(buyStep + 1)} pendingAddress={pendingAddress} />}
                     {buyStep === 3 && <OrderCompleted />}
                 </Grid>
