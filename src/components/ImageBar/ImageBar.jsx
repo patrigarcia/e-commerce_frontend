e6df91f9fec3506bc1214f4fa6e39674151e07d8
patrigarcia <gonzalezgpatricia@hotmail.com>
2023-08-31 20:55:48 +0200

    Added new banner

--- src/components/ImageBar/ImageBar.jsx
+++ src/components/ImageBar/ImageBar.jsx
@@ -5,9 +5,9 @@ import "./ImageBar.scss";
 const ImageBar = () => {
     return (
         <Flex alignItems="center" direction="row" mt={4}>
-            <Box display="flex" bg={"purple"} alignItems="center" w="100%">
+            <Box display="flex" bg={"#2c1299"} alignItems="center" w="100%">
                 <Link href="/promotions">
-                    <Image className="banner" src="src/assets/banner.svg" alt="banner" maxW="100%" height="200px" />
+                    <Image className="banner" src="src/assets/banner.png" alt="banner" maxW="100%" height="200px" />
                 </Link>
             </Box>
         </Flex>
